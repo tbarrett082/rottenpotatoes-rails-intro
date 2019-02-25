b97c4bfcd64a5526056cea3f1f7a8b873407278f
Tanner Barrett <tbarrett082@gmail.com>
2019-02-25 23:26:51 +0000

    change ratings method logic

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,10 +1,5 @@
 class Movie < ActiveRecord::Base
-  def self.ratings
-    ratingsList = []
-    self.all do |p|
-      ratingsList << p.rating
-    end
-    ratingsList = Set[ratingsList]
-    return ratingsList
+  def self.defineRatings
+    return self.pluck(:ratings) 
     
 end
