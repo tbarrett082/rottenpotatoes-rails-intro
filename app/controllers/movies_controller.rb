8c61b9ef5ecc606b11a85868bd2be840f93b91e9
Tanner Barrett <tbarrett082@gmail.com>
2019-02-25 23:35:04 +0000

    Call method and define ratings variable

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -11,6 +11,7 @@ class MoviesController < ApplicationController
   end
 
   def index
+    @all_ratings = Movie.defineRatings
     if params[:sort_by] =='title'
       @movies = Movie.all.order(title: :asc)
       @title_header_color = 'hilite'
